7d0143e374ab2114285f9f4c09d7cb1f4f9bda83
buuoltwo <bullnick20@gmail.com>
2021-09-21 12:03:10 +0800

    feat: Handing states

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Table from './components/Table'
 
+interface IState {
+  brand: {
+    name: string
+    code: number
+    remark?: string
+    isEnabled: boolean
+  }[]
+}
+const initialValue: IState['brand'] = [
+  {
+    name: 'build-your-dream',
+    code: 1,
+    remark: 'Their cars are pretty COOL .',
+    isEnabled: true,
+  },
+]
 function App() {
+  const [brands, setBrands] = useState<IState['brand']>(initialValue)
+  const tableProps = { brands, setBrands }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-header">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Table {...tableProps}></Table>
+        </main>
+        <footer>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </footer>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
