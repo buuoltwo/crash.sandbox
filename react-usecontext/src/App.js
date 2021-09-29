066611776a219d09a95f1aca3f9274d8b606c78e
zhangming <zm@ilng.cc>
2021-09-29 11:42:05 +0800

    init

---
+++ react-usecontext/src/App.js
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion :
+ * @Author       : zhangming
+ * @Date         : 2021-09-29 10:52:16
+ * @LastEditors  : zhangming
+ * @LastEditTime : 2021-09-29 11:40:47
+ */
+import './App.css'
+import Parent from './components/ComponentWithUseContext/Parent'
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-header">
+        <Parent></Parent>
+      </main>
+    </div>
+  )
+}
+
+export default App
