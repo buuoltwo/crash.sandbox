f7557809ab8baf8a0e68371be62549bfb3ba04c1
zhangming <zm@ilng.cc>
2021-10-08 14:38:52 +0800

    feat: returns-callback would be executed before our effects ran
    ```header:'what we log"
    4399
    main contents..
    4399
    main contents..
    4399
    main contents.
    ```

--- react-usecontext/src/components/ComponentWithUseContext/Children/Child1.js
+++ react-usecontext/src/components/ComponentWithUseContext/Children/Child1.js
@@ -3,10 +3,18 @@
  * @Author       : zhangming
  * @Date         : 2021-09-30 09:49:30
  * @LastEditors  : zhangming
- * @LastEditTime : 2021-09-30 11:34:51
+ * @LastEditTime : 2021-10-08 14:30:48
  */
 import Child2 from './Child2'
+import { useEffect } from 'react'
+
 export default function Child1({ count }) {
+  useEffect(() => {
+    console.log(`main contents..`)
+    return () => {
+      console.log(4399)
+    }
+  }, [count])
   return (
     <div className="child-box-1">
       <Child2></Child2>
